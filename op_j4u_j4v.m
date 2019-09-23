688827d1ff1cec96f385d190e8090d16a8de59cc
Niko <niko.m92@gmail.com>
2019-09-23 17:13:33 +0200

    lhs operator
    creation of  operator J4 that simulate the fiber properties

---
+++ op_j4u_j4v.m
@@ -0,0 +1,59 @@
+function varargout = op_j4u_j4v(spu, spv, msh, a_kron_a)
+%   gradu = reshape (spu.shape_function_gradients, spu.ncomp, [], msh.nqn, spu.nsh_max, msh.nel);
+%   gradv = reshape (spv.shape_function_gradients, spv.ncomp, [], msh.nqn, spv.nsh_max, msh.nel);
+  gradu = spu.shape_function_gradients;
+  gradv = spv.shape_function_gradients;
+  ndir = size (gradu, 2);
+
+  rows = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+  cols = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+  values = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+
+  jacdet_weights = msh.jacdet .* msh.quad_weights;
+%   jacdet_weights_mu = jacdet_weights .* mu;
+%   jacdet_weights_lambda = jacdet_weights .* lambda;
+  
+  ncounter = 0;
+  for iel = 1:msh.nel
+    if (all (msh.jacdet(:, iel)))
+      gradu_iel = reshape (gradu(:,:,:,1:spu.nsh(iel),iel), spu.ncomp, ndir, msh.nqn, spu.nsh(iel));
+      epsu_iel = (gradu_iel + permute (gradu_iel, [2 1 3 4]))/2;
+      epsu_iel = reshape (epsu_iel, [spu.ncomp*ndir, msh.nqn * spu.nsh(iel)]);
+%       epsu_iel = repmat (epsu_iel, [1,1,spv.nsh(iel),1]);
+      
+      gradv_iel = reshape (gradv(:,:,:,1:spv.nsh(iel),iel), spv.ncomp, ndir, msh.nqn, spv.nsh(iel));
+      epsv_iel = (gradv_iel + permute (gradv_iel, [2 1 3 4]))/2;
+      epsv_iel = reshape (epsv_iel, [spv.ncomp*ndir, msh.nqn * spv.nsh(iel), 1]);
+%       epsv_iel = repmat (epsv_iel, [1,1,1,spu.nsh(iel)]);
+
+      a_kron_a = reshape(a_kron_a, [ndir * ndir,1])';
+      
+      a_kron_a_epsu = (a_kron_a * epsu_iel)';
+%       a_kron_a_epsu = reshape (a_kron_a_epsu, [spu.ncomp*ndir, msh.nqn, 1, spu.nsh(iel)]);
+
+      a_kron_a_epsv = a_kron_a * epsv_iel;
+%       a_kron_a_epsv = reshape (a_kron_a_epsv, [spv.ncomp*ndir, msh.nqn, spv.nsh(iel), 1]);
+      
+      aux_val = a_kron_a_epsu * a_kron_a_epsv;
+      values = aux_val;
+%       values(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = reshape (sum (aux_val, 2), spv.nsh(iel), spu.nsh(iel));
+      
+      [rows_loc, cols_loc] = ndgrid (spv.connectivity(:,iel), spu.connectivity(:,iel));
+      rows(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = rows_loc;
+      cols(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = cols_loc;
+      ncounter = ncounter + spu.nsh(iel)*spv.nsh(iel);
+    else
+      warning ('geopdes:jacdet_zero_at_quad_node', 'op_su_ev: singular map in element number %d', iel)
+    end
+  end
+  if (nargout == 1 || nargout == 0)
+    varargout{1} = sparse (rows(1:ncounter), cols(1:ncounter), ...
+                           values(1:ncounter), spv.ndof, spu.ndof);
+  elseif (nargout == 3)
+    varargout{1} = rows(1:ncounter);
+    varargout{2} = cols(1:ncounter);
+    varargout{3} = values(1:ncounter);
+  else
+    error ('op_su_ev: wrong number of output arguments')
+  end
+end
---
+++ op_j4u_j4v_tp.m
@@ -0,0 +1,27 @@
+function varargout = op_j4u_j4v_tp(space_u, space_v, msh, a, Ef)
+  A = spalloc (space_v.ndof, space_u.ndof, 5*space_u.ndof);
+  a_kron_a = kron(a',a);
+  
+  for iel = 1:msh.nel_dir(1)
+    msh_col = msh_evaluate_col (msh, iel);
+    sp1_col = sp_evaluate_col (space_u, msh_col, 'value', false, ...
+                               'gradient', true, 'divergence', true);
+    sp2_col = sp_evaluate_col (space_v, msh_col, 'value', false, ...
+                               'gradient', true, 'divergence', true);
+
+    for idim = 1:msh.rdim
+      x{idim} = reshape (msh_col.geo_map(idim,:,:), msh_col.nqn, msh_col.nel);
+    end
+
+    A = A + Ef*op_j4u_j4v(sp1_col, sp2_col, msh_col, a_kron_a);
+  end
+
+  if (nargout == 1)
+    varargout{1} = A;
+  elseif (nargout == 3)
+    [rows, cols, vals] = find (A);
+    varargout{1} = rows;
+    varargout{2} = cols;
+    varargout{3} = vals;
+  end
+end
